2bfadb0d60400034245e56dca1b9d4aa6ec33cb0
Ilya Meerovich <ilya.meerov@gmail.com>
2025-07-10 23:29:08 -0700

    fix: escape CSV values with special characters

--- src/transport/csv.ts
+++ src/transport/csv.ts
@@ -14,7 +14,14 @@ export function exportToCSV(): typeof messages['CSV_EXPORT_ERROR'] | undefined {
     const maxSteeps = clonedSessions.reduce((max, session) => Math.max(max, session.steeps.length), 0)
     const allCustomFields = Array.from(new Set(clonedSessions.flatMap((session) => session.customFields.map((field) => field.name))))
 
-    const headerRow = [
+    const escapeCsvField = (value: string) => {
+      if (value.includes(',') || value.includes('"') || value.includes('\n')) {
+        return `"${value.replace(/"/g, '""')}"` // double-up inner quotes
+      }
+      return value
+    }
+
+    const staticFields = [
       'datetime',
       'brewingVessel',
       'teaName',
@@ -24,6 +31,10 @@ export function exportToCSV(): typeof messages['CSV_EXPORT_ERROR'] | undefined {
       'dryLeaf',
       'wetLeaf',
       'additionalNotes',
+    ] as const
+
+    const headerRow = [
+      ...staticFields,
       ...Array.from({ length: maxSteeps }, (_, i) => `steep-${i + 1}`),
       ...Array.from(new Set(allCustomFields)).map((field) => field),
     ]
@@ -31,22 +42,14 @@ export function exportToCSV(): typeof messages['CSV_EXPORT_ERROR'] | undefined {
     const generateCustomFieldsValuesForSession = (session: Session) => {
       return allCustomFields.map((fieldName) => {
         const existingField = session.customFields.find((field) => field.name === fieldName)
-        return existingField ? existingField.value : ''
+        return existingField ? escapeCsvField(existingField.value) : ''
       })
     }
 
     const rows = clonedSessions.map((session) => {
       return [
-        session.datetime,
-        session.brewingVessel,
-        session.teaName,
-        session.teaProducer,
-        session.origin,
-        session.purchaseLocation,
-        session.dryLeaf,
-        session.wetLeaf,
-        session.additionalNotes,
-        ...Array.from({ length: maxSteeps }, (_, i) => session.steeps[i] || ''),
+        ...staticFields.map((field) => escapeCsvField(session[field])),
+        ...Array.from({ length: maxSteeps }, (_, i) => escapeCsvField(session.steeps[i] || '')),
         ...generateCustomFieldsValuesForSession(session)
       ]
     })
